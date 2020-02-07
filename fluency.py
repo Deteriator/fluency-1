83d8c27e0502599f3009c1b4b84e50eed92de813
Mark Griffith <markgriffith009@Gmail.com>
2020-02-07 14:45:25 +0000

    quick fluency challenge.

--- fluency.py
+++ fluency.py
@@ -1,1 +1,24 @@
+import re
 
+def tripled(lst):
+    return [ num * 3 for num in lst]
+
+def only_negative(lst):
+    return [num < 0 for num in lst]
+ 
+def total(lst):
+     return sum(lst)
+
+def stripped_strings(lst):
+    return [re.sub('\W', '', string) for string in lst]
+
+def find_lovely(lst):
+    if not lst.index('lovely'):
+        return -1
+    return lst.index('lovely')
+    
+def valid_contacts(lst):
+    return [contact for contact in lst if len(contact['phone_number']) == 10]
+
+def contact_names(lst):
+    return [ contact['name'] for contact in lst]
